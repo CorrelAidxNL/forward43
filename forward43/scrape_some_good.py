dcc760a0a865a5fa29312308365e33b94e9ccdf6
Berend van Wijk <sbvanwijk@hotmail.com>
2021-01-14 17:39:54 +0100

    startsomegood scraper update

--- forward43/scrape_some_good.py
+++ forward43/scrape_some_good.py
@@ -7,7 +7,7 @@ import pandas as pd
 browse_url_base        = 'https://startsomegood.com/projects?page='
 browse_pages = []
 
-for page in range(3):
+for page in range(15):
     browse_pages.append(browse_url_base + str(page))
 
 # create dataframe to store data
@@ -35,14 +35,26 @@ for URL in browse_pages:
         project_soup        = BeautifulSoup(project_response.content, "html.parser")
         
         # Get remaining data from project-page soup
-        description         = project_soup.find("div", class_="story").text
-                                        # AttributeError: 'NoneType' object has no attribute 'text'
-        location            = project_soup.find("p", class_="sub-title").text.strip()
-        city                = location.split(',')[0]
-        country             = location  #.split(',')[1]
-                                        # Note that Country and Contact, if working, are identical and messy
-        contact             = location  #.split(',')[1]
-        innovation_type     = project_soup.find('ul', class_="nav nav-pills categories").text
+        if project_soup.find("div", class_="story") is None:
+            description     = 'Description not available'
+        else:   
+            description     = project_soup.find("div", class_="story").text
+        
+        if project_soup.find("p", class_="sub-title") is None:
+            city            = 'City not available'
+            country         = 'Country not available'
+            contact         = 'Contact not available'
+        else:
+            location        = project_soup.find("p", class_="sub-title").text.strip()
+            city            = location.split(',')[0]
+            country         = location  #.split(',')[1]
+                                        # Note that country and contact are identical and messy
+            contact         = location  #.split(',')[1]
+        
+        if project_soup.find('ul', class_="nav nav-pills categories") is None:
+            innovation_type = 'Innovation type not available'
+        else:
+            innovation_type = project_soup.find('ul', class_="nav nav-pills categories").text
         
         good_observation    = {"Title":[title], "Description":[description], "Status":[status], "innovation_type":[innovation_type], "Country":[country], "City":[city], "Contact person/details":[contact], "Link":[link]}
         good_data.append(good_observation)
