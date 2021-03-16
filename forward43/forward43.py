fb70b0e06107f17255b58a34949697df2d3bd8ac
MMAThijssen <mt.marijke@gmail.com>
2021-03-16 22:19:15 +0100

    Start webserver to communicae with GCP

--- forward43/forward43.py
+++ forward43/forward43.py
@@ -1,1 +1,12 @@
 """Main module."""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():         # Replace with real code
+    print('TEST - Run main module')
+    
+if __name__ == '__main__':
+    app.run(port=8080, host='0.0.0.0')
