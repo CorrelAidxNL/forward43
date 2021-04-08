29e3bbdb14fd91331f2e2261b6f97c43d7e42246
MMAThijssen <mt.marijke@gmail.com>
2021-04-08 22:18:05 +0200

    Add statement to return from main

--- forward43/forward43.py
+++ forward43/forward43.py
@@ -5,8 +5,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def main():         # Replace with real code
-    print('TEST - Run main module')
+def main():         # TODO: Replace with real code
+    return 'TEST - Run main module'
     
 if __name__ == '__main__':
     app.run(port=8080, host='0.0.0.0')
