3530e3e5fbe943ad93f5d9e14d657b66556918db
Andrew Sutjahjo <andrewsutjahjo@gmail.com>
2021-06-03 14:49:55 +0200

    Add MasterpeaceScraper.get_survey_responses()

--- forward43/scraper_masterpeace.py
+++ forward43/scraper_masterpeace.py
@@ -1,4 +1,5 @@
 """Scaper for Masterpeace projects."""
+from typing import Dict, List
 
 from surveymonkey import client
 from forward43.scraper import ForwardScraper
@@ -21,6 +22,38 @@ class MasterpeaceScraper(ForwardScraper):
     def scrape(self):
         """Scrape surveys."""
         pass
-        # surveys = self.client.get_survey_lists()
-        #
-        # return surveys
+
+    def get_survey_responses(self, search_string: str) -> List[Dict]:
+        """Get the correct surveys for Masterpeace projects."""
+        surveys = self.client.get_survey_lists()
+        if 'data' not in surveys:
+            raise ValueError(
+                f"Surveys not found for search string {search_string}, data returned: {surveys}"
+            )
+
+        scrapable_surveys = {}
+        for survey in surveys["data"]:
+            if search_string in survey["title"]:
+                scrapable_surveys[survey["id"]] = survey
+
+        if len(scrapable_surveys) == 0:
+            raise ValueError(
+                f"Surveys not found for search string {search_string}, data returned: {surveys}"
+            )
+
+        returnable_responses = {}
+        for survey_id in scrapable_surveys.keys():
+            returnable_responses[survey_id] = {}
+            responses = self.client.get_all_pages_response(survey_id)
+            for response in responses:
+                if not response.get("data", []):
+                    raise ValueError(
+                        f"Responses not found for survey_id {survey_id}, data returned: {responses}"
+                    )
+                for response_data in response.get("data"):
+                    respondant_id = response_data.get("id", "")
+                    returnable_responses[survey_id][respondant_id] = response_data
+
+        return returnable_responses
+
+        return returnable_surveys
