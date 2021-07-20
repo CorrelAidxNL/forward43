39156f67bebd0b83841102b91c3666fe587c5b7d
MMAThijssen <mt.marijke@gmail.com>
2021-07-20 21:15:27 +0200

    Test scraper_linkedin

---
+++ tests/test_scraper_linkedin.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Tests for LinkedIn getter. 
+
+Created on Sun May 30 20:30:20 2021
+@author: Marijke Thijssen
+"""
+# %%   Import packages and settings
+import unittest
+from selenium import webdriver
+
+from forward43 import scraper_linkedin
+  
+
+# %%   Functions
+class TestLinkedInScraper(unittest.TestCase):
+  
+    def setUp(self):
+        self.scraper = scraper_linkedin.LinkedInScraper()
+    
+    def test_GoToLinkedin(self):
+        self.browser = webdriver.Chrome(executable_path='data\\chromedriver_win32\\chromedriver.exe')
+        self.browser.get('https://www.linkedin.com/uas/login')
+        self.assertIn('LinkedIn', self.browser.title)
+    
+    
+    def test_FindLoginFields(self):
+        try:
+            self.browser.find_element_by_id('username')
+            self.browser.find_element_by_id('password')
+            elements_exist = True
+        except:
+            elements_exist = False
+        self.assertTrue(elements_exist)
+        
+  
+    def tearDown(self):
+        self.browser.close()
+  
+    
+# %%   If-Main
+if __name__ == '__main__':
+    unittest.main()
