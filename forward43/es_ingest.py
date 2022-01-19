269cb1271f556592cdd2f09d9f92325508ce469f
akashrajkn <akashrajkn@gmail.com>
2022-01-19 17:13:14 +0100

    Created separate functions for localhost and remote connections

--- forward43/es_ingest.py
+++ forward43/es_ingest.py
@@ -7,32 +7,33 @@ from forward43.logger         import logger
 import forward43.utils.elasticsearch as es_utils
 
 
-def load_to_es(filepath, es_object): 
+def injest_data_to_es(es_host, es_port, es_user, es_secret):
     '''
-    Load json data into elastic
-    @filepath  : json filepath which contains project details
-    @es_object : es instance
+    Load json data and Injest into elastic
+    @host : elasticsearch host
+    @port : elasticsearch port
     '''
-    try:
-        with open(filepath) as f:
-            data = json.load(f)
+    if es_host == 'localhost':
+        es_object = es_utils.connect_elasticsearch_localhost(host=es_host, port=es_port)
+    else:
+        es_object = es_utils.connect_elastic_remote(host=es_host, port=es_port, user=es_user, secret=es_secret)
 
-        for document in data:
-            es_utils.index_document(es_object, 'forward43', '_doc', document, document['id'])
+    # injest scraped data
+    for filepath in os.listdir(DATA_DIRECTORY):
+        if filepath.endswith('.json'):
+            load_to_es(os.path.join(DATA_DIRECTORY, filepath), es_object)
 
-    except Exception as e:
-        logger.exception(f'Error while reading {filepath}. Message: {e}')
+        try:
+            with open(filepath) as f:
+                data = json.load(f)
+
+            for document in data:
+                es_utils.index_document(es_object, 'forward43', '_doc', document, document['id'])
+
+        except Exception as e:
+            logger.exception(f'Error while reading {filepath}. Message: {e}')
 
 
 if __name__ == '__main__':
 
-    # Get ES instance
-    es_object = es_utils.connect_elasticsearch()
-
-    # Populate ES with data
-    for filepath in os.listdir(DATA_DIRECTORY):
-        if not filepath.endswith('.json'):
-            continue
-
-        logger.info(f'file: {filepath}')
-        load_to_es(os.path.join(DATA_DIRECTORY, filepath), es_object)
+    injest_data_to_es(es_host='localhost', es_port=9200)
